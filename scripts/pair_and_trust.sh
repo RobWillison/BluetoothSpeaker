a8303ec0a9d9520dbc1c66427373b6e1c6289fd2
Rob <rob@pop-os.localdomain>
2021-09-03 19:23:35 +0100

    test

--- scripts/pair_and_trust.sh
+++ scripts/pair_and_trust.sh
@@ -5,7 +5,7 @@ chmod 777 expect_script.log
 sleep 2
 
 echo "Trusting and connecting.."
-device_mac_address=$(cat expect_script.log | grep -Pom 1 "(?<=Device ).*(?= Connected)")
+device_mac_address=$(cat expect_script.log | grep -Pom 1 "(?<=Device ).*(?= )")
 echo mac address is $device_mac_address
 if [[ ! -z $device_mac_address ]] ; then
             expect trust_and_connect.expect $device_mac_address
