f6939d2e1f01f2fc5f43a6f2c6d85986c6e31172
Rob <rob@pop-os.localdomain>
2021-09-03 14:08:16 +0100

    test

--- skip_encoder.py
+++ skip_encoder.py
@@ -1,7 +1,7 @@
 from encoder import Encoder
 
 class SkipEncoder:
-    def __init__(leftPin, rightPin, player):
+    def __init__(self, leftPin, rightPin, player):
         self.last_tick = None
         self.tick_count = 0
         self.encoder = Encoder(leftPin, rightPin, self.onChange)
