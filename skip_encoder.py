7bd9ef52ebf3590848dfc42c3c64be1e63e309c0
Rob <rob@pop-os.localdomain>
2021-09-03 14:22:23 +0100

    test

--- skip_encoder.py
+++ skip_encoder.py
@@ -1,14 +1,18 @@
 from encoder import Encoder
 import time
 
-class SkipEncoder:
-    def __init__(self, leftPin, rightPin, player):
+class SkipNStettingEncoder:
+    def __init__(self, leftPin, rightPin, button, player):
         self.last_tick = 0
         self.tick_count = 0
         self.encoder = Encoder(leftPin, rightPin, self.onChange)
         self.player = player
+        self.mode = 'SKIP'
 
-    def onChange(self, value, direction):
+        GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(button, GPIO.FALLING, callback=p.togglePaused)
+
+    def handleSkip(self, direction):
         now = time.time()
         if now > self.last_tick + 1:
             self.tick_count = 0
@@ -16,7 +20,6 @@ class SkipEncoder:
         self.last_tick = now
         self.tick_count += direction
 
-        print(self.tick_count)
         if self.tick_count < -10:
             self.tick_count = 0
             self.player.next()
@@ -26,3 +29,7 @@ class SkipEncoder:
             self.tick_count = 0
             self.player.prev()
             print('Prev')
+
+    def onChange(self, value, direction):
+        if self.mode == 'SKIP':
+            handleSkip(direction)
--- test.py
+++ test.py
@@ -3,24 +3,20 @@ import RPi.GPIO as GPIO
 import time
 from player import Player
 from display import Display
-from skip_encoder import SkipEncoder
+from skip_encoder import SkipNStettingEncoder
 
 def encoderMoved(current, change):
     print(change)
 
 d = Display()
 p = Player(d.trackChanged, d.pausedStatusChanged)
-e = SkipEncoder(4, 22, p)
+e = SkipNStettingEncoder(4, 22, 17, p)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(27, GPIO.OUT)
 GPIO.output(27, GPIO.HIGH)
-
-pausePlayButton = 17
-GPIO.setup(pausePlayButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(pausePlayButton, GPIO.FALLING, callback=p.togglePaused)
 
 while True:
     time.sleep(1)
