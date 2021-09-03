8f49a83892fd2d78e1037ad947fd7338e6561ae3
Rob <rob@pop-os.localdomain>
2021-09-03 13:48:44 +0100

    test

--- display.py
+++ display.py
@@ -21,14 +21,14 @@ class Display:
     def trackChanged(self, track, artist, album):
         self.lcd.clear()
         self.lcd.setCursor(0,0)
-        self.lcd.printout(self.cropText(track) + u"\u23F8")
+        self.lcd.printout(self.cropText(track))
         self.lcd.setCursor(0,1)
         self.lcd.printout(self.cropText(artist))
 
     def pausedStatusChanged(self, paused):
         if paused:
-            self.lcd.setCursor(14, 1)
+            self.lcd.setCursor(15, 1)
             self.lcd.printCustomSymbol(1)
         else:
-            self.lcd.setCursor(14, 1)
+            self.lcd.setCursor(15, 0)
             self.lcd.printout(' ')
